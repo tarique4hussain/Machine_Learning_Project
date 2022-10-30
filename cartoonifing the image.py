5e4562945663c12a6d45c999111701ddc2534b60
Tarique Hussain <73169319+tarique4hussain@users.noreply.github.com>
2022-10-30 18:35:56 +0530

    Update cartoonifing the image.py

--- cartoonifing the image.py
+++ cartoonifing the image.py
@@ -26,7 +26,7 @@ def cartoonify(ImagePath):
     # read the image
     originalmage = cv2.imread(ImagePath)
     originalmage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2RGB)
-    #print(image)  # image is stored in form of numbers
+    
 
     # confirm that image is chosen
     if originalmage is None:
@@ -34,20 +34,17 @@ def cartoonify(ImagePath):
         sys.exit()
 
     ReSized1 = cv2.resize(originalmage, (960, 540))
-    #plt.imshow(ReSized1, cmap='gray')
-
+   
 
     #converting an image to grayscale
     grayScaleImage= cv2.cvtColor(originalmage, cv2.COLOR_BGR2GRAY)
     ReSized2 = cv2.resize(grayScaleImage, (960, 540))
-    #plt.imshow(ReSized2, cmap='gray')
-
+    
 
     #applying median blur to smoothen an image
     smoothGrayScale = cv2.medianBlur(grayScaleImage, 5)
     ReSized3 = cv2.resize(smoothGrayScale, (960, 540))
-    #plt.imshow(ReSized3, cmap='gray')
-
+    
     #retrieving the edges for cartoon effect
     #by using thresholding technique
     getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255, 
@@ -55,21 +52,18 @@ def cartoonify(ImagePath):
         cv2.THRESH_BINARY, 9, 9)
 
     ReSized4 = cv2.resize(getEdge, (960, 540))
-    #plt.imshow(ReSized4, cmap='gray')
-
+    
     #applying bilateral filter to remove noise 
     #and keep edge sharp as required
     colorImage = cv2.bilateralFilter(originalmage, 9, 300, 300)
     ReSized5 = cv2.resize(colorImage, (960, 540))
-    #plt.imshow(ReSized5, cmap='gray')
-
+    
 
     #masking edged image with our "BEAUTIFY" image
     cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge)
 
     ReSized6 = cv2.resize(cartoonImage, (960, 540))
-    #plt.imshow(ReSized6, cmap='gray')
-
+   
     # Plotting the whole transition
     images=[ReSized1, ReSized2, ReSized3, ReSized4, ReSized5, ReSized6]
 
